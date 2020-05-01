d2c7a986fd9fdca6e8227be616cb1d89bf9cccb1
Mo Al <moal@Mos-MacBook-Pro.local>
2020-04-30 21:51:27 -0700

    json data loading in properly

--- web-app/app.py
+++ web-app/app.py
@@ -1,9 +1,14 @@
+import json
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('APP_CONFIG_FILE', silent=True)
 
+with open("./src/initial_pull.json", "r") as file:
+    full_data = json.load(file)
+
 @app.route("/")
 def index():
-  return render_template("./triton_news_header.html")
+  return render_template("./table.html", data=full_data)
+  # return render_template("./triton_news_header.html")
