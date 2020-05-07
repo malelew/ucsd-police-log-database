4fa84990fa93b6500c6023fd56663f112e6a7e18
Mo Al <moal@Mos-MacBook-Pro.local>
2020-05-07 01:20:56 -0700

    renamed req.txt

---
+++ setup.py
@@ -0,0 +1,27 @@
+# This file may be used to create an environment using:
+# $ conda create --name <env> --file <this file>
+# platform: osx-64
+ca-certificates=2020.4.5.1=hecc5488_0
+certifi=2020.4.5.1=py38h32f6830_0
+click=7.1.2=pyh9f0ad1d_0
+flask=1.1.2=pyh9f0ad1d_0
+flask-sqlalchemy=2.4.1=pyh9f0ad1d_0
+itsdangerous=1.1.0=py_0
+jinja2=2.11.2=pyh9f0ad1d_0
+libcxx=10.0.0=h1af66ff_2
+libffi=3.2.1=h4a8c4bd_1007
+markupsafe=1.1.1=py38h64e0658_1
+ncurses=6.1=h0a44026_1002
+openssl=1.1.1g=h0b31af3_0
+pip=20.1=pyh9f0ad1d_0
+python=3.8.2=hd5f0129_7_cpython
+python_abi=3.8=1_cp38
+readline=8.0=hcfe32e1_0
+setuptools=46.1.3=py38h32f6830_0
+sqlalchemy=1.3.16=py38h64e0658_0
+sqlite=3.30.1=h93121df_0
+tk=8.6.10=hbbe82c9_0
+werkzeug=1.0.1=pyh9f0ad1d_0
+wheel=0.34.2=py_1
+xz=5.2.5=h0b31af3_0
+zlib=1.2.11=h0b31af3_1006
